187f5dc1a410332850b946a5d7509c67b463ca9a
Tobias Bengfort <tobias.bengfort@posteo.de>
2020-01-26 13:04:19 +0100

    fix some infos in setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ from setuptools import find_packages, setup
 setup(
     name='django-bs4',
     description='simple bootstrap4 support for django',
+    long_description=open('README.rst').read(),
     version='0.0.1',
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Environment :: Web Environment',
         'Framework :: Django',
     ],
